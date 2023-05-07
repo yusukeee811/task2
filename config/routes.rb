d70e3b7063c15d0c8f0ebd9e473969f8cd887e14
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-05-07 05:21:24 +0000

    [Update]topページhtml作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'books' => 'books#index', as: 'book'
+  get 'books/show'
+  get 'books/edit'
 end
